b8a6d0f92a26a5a9201c7b71813ffd2402948622
liuzhongiceman <zhongliu884@gmail.com>
2018-01-29 20:15:10 -0800

    changed

---
+++ app/config/connection.js
@@ -0,0 +1,17 @@
+// Dependencies
+var Sequelize = require("sequelize");
+
+// Creates mySQL connection using Sequelize
+var sequelize = new Sequelize("sequelize_library", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  port: "8889"
+});
+
+// Exports the connection for other files to use
+module.exports = sequelize;
