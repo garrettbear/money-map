94a6087ec41c2cbb8c187ec0b83149d407395adc
Nathan Chen <chenl.nathan@gmail.com>
2018-01-30 16:55:37 -0800

    auth testing setup

---
+++ money_test_nathan/authTesting/server.js
@@ -0,0 +1,37 @@
+//Dependencies
+// =============================================================
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+
+//Setup app
+// =============================================================
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Setup handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static("public")); //Gives handlebars access to the public folder.
+
+//Setup Passport
+// =============================================================
+app.use(session({ secret: 'the login creation is nigh',resave: true, saveUninitialized:true})); // session secret 
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+
+// Routes
+// =============================================================
+require("./routes/auth_routes.js")(app);
+
+// Start App
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
