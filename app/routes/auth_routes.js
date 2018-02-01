1da3da59e1fe7691e25f9bc418c475b0e5602b85
Garrett Bear <gwhisten@gmail.com>
2018-01-31 23:52:58 -0800

    modal button added to submit expense

--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -1,19 +1,22 @@
-module.exports = function(app, passport){
-    //Respond with pages
-    app.get('/', function(req,res) {
-      res.render('index');
+module.exports = function (app, passport) {
+  //Respond with pages
+  app.get('/', function (req, res) {
+    res.render('index');
   });
 
-    app.get('/signup', function(req,res) {
-        res.render('signup');
-    });
-    app.get('/signin', function(req,res) {
-        res.render('signin');
-    });
+  app.get('/dashboard', function (req, res) {
+    res.render('dashboard');
+  });
 
-    app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect: '/dashboard',
-            failureRedirect: '/signup'
-        } 
-    ));
+  app.get('/signup', function (req, res) {
+    res.render('signup');
+  });
+  app.get('/signin', function (req, res) {
+    res.render('signin');
+  });
+
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/signup'
+  }));
 }
