134969ad3c3e8ab274ad214f8dc148dbab43e0ad
Nathan Chen <chenl.nathan@gmail.com>
2018-02-03 11:44:56 -0800

    fixed redirect after a failed signup or signin

--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -19,11 +19,11 @@ module.exports = function (app, passport) {
   
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/dashboard',
-    failureRedirect: '/signup'
+    failureRedirect: '/#create-account-tab'
   }));
   app.post('/signin', passport.authenticate('local-signin', {
     successRedirect: '/dashboard',
-    failureRedirect: '/signin'
+    failureRedirect: '/#login-tab'
     }
   ));
 }
